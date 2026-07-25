ddc0dd50a1de3510e644d5a5ffa9a3458a5ee6a9
aivicdemo <aivicdemo@gmail.com>
2026-07-25 14:47:42 +0900

    chore: bootstrap test_conformance attempt 1/5

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
-  testMatch: "**/itg-1-*.spec.ts",
+  testMatch: "**/common-*.spec.ts",
   workers: 4,
   use: { baseURL: process.env.PLAYWRIGHT_BASE_URL || "https://dev.dblpbeqrl18h6.amplifyapp.com" },
   reporter: "list",
