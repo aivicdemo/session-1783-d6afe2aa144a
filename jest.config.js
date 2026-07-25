79888eae75607941a8f4ff9bd229301ec2c9705c
aivicdemo <aivicdemo@gmail.com>
2026-07-25 13:44:59 +0900

    chore: bootstrap test_conformance attempt 1/5

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  testMatch: ["**/tests/unit/itg-6-*.test.ts","**/tests/unit/itg-6-*.test.js"],
+  testMatch: ["**/tests/unit/itg-1-*.test.ts","**/tests/unit/itg-1-*.test.js"],
   transform: { "^.+\\.tsx?$": "ts-jest" },
   testEnvironment: "node",
 };
